190d60ce61103939333ef06750dc1300eda993dc
c0rdeiro <franciscordeiro99@gmail.com>
2023-03-18 16:21:06 +0000

    simple contract creation

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.18;
+import "hardhat/console.sol";
+
+contract WavePortal{
+   constructor() {
+        console.log("This is where it began.");
+    }
+}
